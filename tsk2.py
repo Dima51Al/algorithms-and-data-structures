26d5f110102eba72355866ec8d926ba9365919cc
Dima51AL <dmitry290306@gmail.com>
2024-09-18 10:05:14 +0300

    итог

--- tsk2.py
+++ tsk2.py
@@ -2,3 +2,4 @@ a, b = (input()).split()
 a, b = int(a), int(b)
 
 print(a + b**2)
+
--- tsk4.py
+++ tsk4.py
@@ -2,3 +2,4 @@ a, b = open("input.txt").read().split()
 a, b = int(a), int(b)
 
 open("output.txt", "w").write(str(a+b**2))
+
