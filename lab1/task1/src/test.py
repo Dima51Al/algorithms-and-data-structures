54bb674687df48c7bbbb47bbbafa9fd77b362809
Dima51AL <dmitry290306@gmail.com>
2024-09-26 13:01:58 +0300

    Проведены тесты на заданных ограничениях

--- lab1/task1/src/test.py
+++ lab1/task1/src/test.py
@@ -1,10 +1,19 @@
 import random
 import time
 from main import insertionSort
+
+def normVid(array: list) -> str:
+    s = ""
+    for i in range(len(array) - 1):
+        s += str(array[i]) + " "
+    s += str(array[-1])
+    return s
+
+
 array = [random.randint(-10**9, 10**9) for i in range(10**3)]
 tmp = time.time()
-
-
+string = "1000"+"\n" + normVid(array)
+open("input.txt", "w").write(string)
 
 insertionSort()
 print(time.time() - tmp, "seconds")
