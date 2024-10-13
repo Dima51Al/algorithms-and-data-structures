4b7dad9ddd33b54b465958975958e99d3c6aee46
Dima51AL <dmitry290306@gmail.com>
2024-10-13 17:25:45 +0300

    тест2 идентичный псевдокоду

--- lab2/task1/scr/main_tmp.py
+++ lab2/task1/scr/main_tmp.py
@@ -1,27 +1,36 @@
 def merge(array, p, q, r):
-    n1 = q - p
+    n1 = q - p + 1
     n2 = r - q
 
-    L = [i for i in range(n1)]
-    R = [i for i in range(n2)]
+    L = [0 for _ in range(1, n1 + 1)]
+    R = [0 for _ in range(1, n2 + 1)]
 
     for i in range(n1):
-        L[i] = array[p + i]
+        L[i] = array[p + i - 1]
 
-    for i in range(n2):
-        R[i] = array[q + i]
-
-
-    print(L, R)
+    for j in range(n2):
+        R[j] = array[q + j]
+    L.append(1000000000)
+    R.append(1000000000)
     i, j = 0, 0
-    for k in range(p, r-1):
+
+    for k in range(p, r):
+        # if len(L) == i:
+        #     array[k] = R[j]
+        #     j += 1
+        #     break
+        # if len(R) == j:
+        #     array[k] = L[i]
+        #     i += 1
+        #     break
+
         if L[i] <= R[j]:
             array[k] = L[i]
             i += 1
         else:
             array[k] = R[j]
             j += 1
-
+    print(array, p, q, r)
 
 def merge_sort(array: list[int], p, r):
     if p < r:
@@ -32,6 +41,11 @@ def merge_sort(array: list[int], p, r):
         return array
 
 
+
+
 a = [i for i in range(16, 0, -1)]
-print(merge_sort(a, 0, 16))
+
+print(a)
+merge_sort(a, 0, 16)
+print(a)
 
