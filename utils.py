2cb01308c50f9d04b7a467de7846c12ec11929c5
Dima51AL <dmitry290306@gmail.com>
2024-10-26 11:39:43 +0300

    reformat the file

--- utils.py
+++ utils.py
@@ -9,17 +9,19 @@ path_output = "../txtf/output.txt"
 def write_file(string: str) -> None:
     with open(path_output, "w", encoding="utf-8") as file:
         file.write(string)
+
+
 def read_file() -> str:
     with open(path_input, "r") as file:
         return file.read()
+
+
 def normVid(array: list) -> str:
     s = ""
     for i in range(len(array) - 1):
         s += str(array[i]) + " "
     s += str(array[-1])
     return s
-
-
 
 
 def read_file_line(path: str, num: int) -> str:
@@ -44,6 +46,7 @@ def base_test(function, *args):
     print("Использование памяти: ", memory_after - memory_before, "МБ")
     return answer
 
+
 def min_max(array: list[int], mm: int) -> list[int]:
     """ массив; 1 если мин, -1 если макс | выход: [элемент, индекс]"""
     if len(array) == 0:
@@ -55,4 +58,3 @@ def min_max(array: list[int], mm: int) -> list[int]:
         if mm * array[i] < mm * array1[0]:
             array1[0], array1[1] = array[i], i
     return array1
-
