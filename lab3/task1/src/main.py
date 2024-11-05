72203daac7431f9af7833754a1d2d47338cb530d
Dima51AL <dmitry290306@gmail.com>
2024-11-05 22:34:22 +0300

    task1 randomize sort fail_recursion

--- lab3/task1/src/main.py
+++ lab3/task1/src/main.py
@@ -1,1 +1,39 @@
-# main.py
+from random import randint
+
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+
+def partition(array, left, right):
+    x = array[left]
+    j = left
+    for i in range(left + 1, right):
+        if array[i] <= x:
+            j = j + 1
+            swap(array, j, i)
+    swap(array, left, j)
+    return j
+
+
+def quicksort(array, left, right):
+    if left < right:
+        m = partition(array, left, right)
+        quicksort(array, left, m)
+        quicksort(array, m + 1, right)
+
+
+def randomize_quicksort(array, left, right):
+    if left < right:
+        key = randint(left, right-1)
+        swap(array, left, key)
+        m = partition(array, left, right)
+        quicksort(array, left, m)
+        quicksort(array, m + 1, right)
+    return array
+
+
+if __name__ == '__main__':
+    array = [5, 4, 3, 2, 1]
+    randomize_quicksort(array, 0, len(array))
+    print(array)
