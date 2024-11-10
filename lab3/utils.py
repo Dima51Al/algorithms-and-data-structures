729d6faa4eb2bf86e9a515852939724bdb56d706
Dima51AL <dmitry290306@gmail.com>
2024-11-10 15:24:09 +0300

    utils.py updated

--- lab3/utils.py
+++ lab3/utils.py
@@ -43,7 +43,7 @@ def base_test(function, *args):
     memory_after = memory_profiler.memory_usage()[0]
 
     print()
-    print("Время выполнения: ", tmp)
+    print("Время выполнения: ", tmp, " сек")
     print("Использование памяти: ", memory_after - memory_before, "МБ")
     print()
     return answer
@@ -66,3 +66,36 @@ def random_array(length: int, min_value: int, max_value: int) -> list[int]:
     return [random.randint(min_value, max_value) for _ in range(length)]
 
 
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+
+def partition(array, left, right):
+    x = array[left]
+    less_then = left
+    grow_then = right
+    i = left + 1
+
+    while i <= grow_then:
+        if array[i] < x:
+            swap(array, less_then, i)
+            less_then += 1
+            i += 1
+        elif array[i] > x:
+            swap(array, grow_then, i)
+            grow_then -= 1
+        else:
+            i += 1
+    return grow_then, less_then
+
+
+def randomize_quicksort(array, left, right):
+    # Рекурсивный вызов для подмассивов
+
+    if left < right:
+        key = random.randint(left, right - 1)
+        swap(array, left, key)
+
+        grow_then, less_then = partition(array, left, right)
+        randomize_quicksort(array, left, less_then - 1)
+        randomize_quicksort(array, grow_then + 1, right)
