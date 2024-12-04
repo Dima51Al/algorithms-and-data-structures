b14e35ef7f969f98fc37206b2041717802e11ae6
dmitry_windows <dmitry290306@gmail.com>
2024-12-04 14:53:45 +0300

    task3 completed

--- lab5/task3/src/main.py
+++ lab5/task3/src/main.py
@@ -55,8 +55,6 @@ class QueueModified:
             self.duration = duration
             self.right = right
 
-
-
     queue_array: set[Elem] = set()
 
     first: Elem = None
@@ -68,16 +66,18 @@ class QueueModified:
     def minus_heart(self):
         self.first.count_of_heartbroken -= 1
 
-    def set_limit(self, limit=10**6):
+    def set_limit(self, limit=10 ** 6):
         self.queue_limit = limit
 
     def isEmpty(self) -> bool:
         return self.length_queue == 0
 
     def push(self, duration):
-        a = False
+
         if self.length_queue == self.queue_limit:
-            a = True
+            self.last.count_of_heartbroken += 1
+
+            return
 
         elem = self.Elem(None, duration, None)
 
@@ -91,9 +91,6 @@ class QueueModified:
             self.last.right = elem
             self.last = elem
             self.queue_array.add(elem)
-
-        if a:
-            self.last.count_of_heartbroken += 1
 
         self.length_queue += 1
 
@@ -110,27 +107,18 @@ class QueueModified:
             return
 
         if self.length_queue == 1:
-            answer = self.first.start
-
-            if self.first.count_of_heartbroken != 0:
-                self.minus_heart()
-                return -1
+            answer = [self.first.start, self.first.count_of_heartbroken]
 
             self.queue_array.remove(self.first)
             self.length_queue -= 1
             self.last = None
             self.first = None
 
-
             return answer
 
         #     else
 
-        answer = self.first.start
-
-        if self.first.count_of_heartbroken != 0:
-            self.minus_heart()
-            return -1
+        answer = [self.first.start, self.first.count_of_heartbroken]
 
         self.first = self.first.right
         self.queue_array.remove(self.first.left)
@@ -144,6 +132,44 @@ class QueueModified:
             return
         self.first.finish = self.first.duration + msec
         self.first.start = msec
+
+    def get_array(self) -> list:
+        arr = []
+        if self.isEmpty():
+            return arr
+
+        elem = self.first
+
+        while elem.right is not None:
+            arr.append([elem.duration, elem.count_of_heartbroken])
+            elem = elem.right
+
+        arr.append([elem.duration, elem.count_of_heartbroken])
+
+        return arr
+
+    def check_time(self, time):
+        if self.isEmpty():
+            return
+        answer = list()
+
+        while not self.isEmpty():
+            if time == self.first.finish:
+
+                value = self.pop()
+                decoder(value, answer)
+
+                self.init_after_start(time)
+            else:
+                break
+        return answer
+
+
+def decoder(arr1: list, array: list):
+    array.append(arr1[0])
+    if arr1[1] != 0:
+        for i in range(arr1[1]):
+            array.append(-1)
 
 
 def main(array: list[list], S):
@@ -160,48 +186,41 @@ def main(array: list[list], S):
     INDEX = 0
     milisec = 0
 
-    while milisec < 10 ** 7:
-
-        while (queue.first is not None) and (milisec == queue.first.finish):
-            answer_array_main.append(queue.pop())
-            queue.init_after_start(milisec)
-            if queue.first is None:
-                break
+    while milisec < 17:
+        tmp_arr = queue.check_time(milisec)
+        if tmp_arr is not None:
+            answer_array_main += tmp_arr
 
         if INDEX < len(array):
-            elem = array[INDEX]
-            while elem[0] == milisec and INDEX < len(array):
-                elem = array[INDEX]
-                queue.push(elem[1])
+            while array[INDEX][0] == milisec:
+
+                queue.push(array[INDEX][1])
+
                 if queue.length_queue == 1:
                     queue.init_after_start(milisec)
+
                 INDEX += 1
+                if INDEX == len(array):
+                    break
 
-
-        while (queue.first is not None) and (milisec == queue.first.finish):
-            answer_array_main.append(queue.pop())
-            queue.init_after_start(milisec)
-            if queue.first is None:
-                break
-
+        tmp_arr = queue.check_time(milisec)
+        if tmp_arr is not None:
+            answer_array_main += tmp_arr
 
         milisec += 1
+
     return answer_array_main
 
 
-
-
-
 if __name__ == '__main__':
-    from lab5.utils import read_file_line, write_file
+    from lab5.utils import read_file_line, write_file, vertical_norm_view
 
     path_input = os.path.join(os.path.dirname(__file__), '..', 'txtf', 'input.txt')
     path_output = os.path.join(os.path.dirname(__file__), '..', 'txtf', 'output.txt')
 
     S, n = list(map(int, read_file_line(path_input, 0).split()))
 
-    array = [list(map(int, read_file_line(path_input, i+1).split())) for i in range(n)]
+    array = [list(map(int, read_file_line(path_input, i + 1).split())) for i in range(n)]
     answer_array = main(array, S)
 
-    write_file(path_output, str(answer_array))
-
+    write_file(path_output, vertical_norm_view(answer_array))
